6543f9478b581f704f54f3b2194a54174f5e8c9a
furkanhaliler <furkaner23@gmail.com>
2023-06-05 00:01:42 +0300

    -Added HomePage and UserPage. -Added React Router.

--- src/container/App.js
+++ src/container/App.js
@@ -3,16 +3,27 @@ import ApiProgress from "../shared/ApiProgress";
 import UserSignupPage from "../pages/UserSignupPage";
 import LoginPage from "../pages/LoginPage";
 import LanguageSelector from "../components/LanguageSelector";
+import HomePage from "../pages/HomePage";
+import UserPage from "../pages/UserPage";
+import {
+  HashRouter,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
-    <div className="row">
-      <div className="col">
-        <UserSignupPage />
-      </div>
-      <div className="col">
-        <LoginPage />
-      </div>
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/login" component={LoginPage}></Route>
+          <Route path="/signup" component={UserSignupPage}></Route>
+          <Route path="/user/:username" component={UserPage}></Route>
+          <Redirect to="/"></Redirect>
+        </Switch>
+      </HashRouter>
       <LanguageSelector></LanguageSelector>
     </div>
   );
