1bbb0bbb4324e5717cec3717375ea8b14f76c9c2
furkanhaliler <furkaner23@gmail.com>
2023-07-13 21:52:37 +0300

    -Added SecureLs library and configured local storage.

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,5 +1,9 @@
 import { createStore } from "redux";
 import authReducer from "./AuthReducer";
+import SecureLS from "secure-ls";
+
+const secureLs = new SecureLS();
+
 const defaultState = {
   isLoggedIn: false,
   username: undefined,
@@ -8,8 +12,35 @@ const defaultState = {
   password: undefined,
 };
 
+const getStateFromStorage = () => {
+  const hoaxAuth = secureLs.get("hoax-auth");
+
+  let stateInLocalStorage = {
+    isLoggedIn: false,
+    username: undefined,
+    displayName: undefined,
+    image: undefined,
+    password: undefined,
+  };
+
+  if (hoaxAuth) {
+    stateInLocalStorage = hoaxAuth;
+  }
+  return stateInLocalStorage;
+};
+
+const updateStateInStorage = (newState) => {
+  secureLs.set("hoax-auth", newState);
+};
+
 const configureStore = () => {
-  return createStore(authReducer, defaultState);
+  const store = createStore(authReducer, getStateFromStorage());
+
+  store.subscribe(() => {
+    updateStateInStorage(store.getState());
+  });
+
+  return store;
 };
 
 export default configureStore;
